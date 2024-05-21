638b429257b61f10d98dc23e012cf0814a1fe05c
GhostcCoder <116057784+GhostcCoder@users.noreply.github.com>
2024-05-21 14:07:24 +0900

    민석 - 5.21

--- lib/Screens/inject/inject_update.dart
+++ lib/Screens/inject/inject_update.dart
@@ -127,15 +127,12 @@ class _inject_UpdateState extends State<injectUpdate> {
                         value: Change == 1,
                         onChanged: (value) {
                           setState(() {
-                            if (value != null) {
-                              Change = value ? 1 : 0;
-                            }
+                            Change = 0;
                           });
                         }),
                     Text("추가 교체 여부"),
                   ],
                 ),
-
                 SizedBox(height: 20),
                 addAlram(), //알람 추가 버튼
                 //SizedBox(height: 30),
